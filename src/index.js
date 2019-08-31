d223ad24a6badc8d9534ae6899e65bcd7b292258
gsalac7 <gsalac7@gmail.com>
2019-08-31 15:18:14 -0700

    Changed bit_value

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 // function that builds a grid in the "container"
-const bit_value = 50;
+const bit_value = 32;
 function createGrid(x) {
     for (let rows = 0; rows < x; rows++) {
         for (let columns = 0; columns < x; columns++) {
             $("#grid-container").append("<div class='grid'></div>");
         };
-    };
+    };;
     $(".grid").width(900/x);
-    $(".grid").height(900/x);
+    $(".grid").height(900/x)
 };
+
+function get_color() {
+
+}
 
 $(document).ready(function() {
     createGrid(bit_value);
-    $(".grid").hover(function() {
+    $(".grid").mousedown(function() {
         $(this).css("background-color", "black");
     });
 });
